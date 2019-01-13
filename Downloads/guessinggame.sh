ebfa1b5beeeb09ed18691b361528817c1d6b7b78
legacyunited <sahil.universe@gmail.com>
2019-01-13 18:09:42 -0500

    guessing game full version 1.0

---
+++ Downloads/guessinggame.sh
@@ -0,0 +1,24 @@
+files=4
+test=0
+echo "Welcome to Guessing Game!"
+
+while [[ test -eq 0 ]]
+do
+	echo "Can you guess how many files are in the current directory? (Hint: You have infinite guesses.)"
+	read response
+	if [[ response -eq $files ]]
+	then
+		echo "You got it!"
+		echo "And, thanks for playing!"
+		test=1
+	elif [[ response -lt $files ]] 
+	then
+		echo "Try guessing higher"
+	elif [[ response -gt $files ]]
+	then
+		echo "Try guessing lower"
+	fi
+done
+
+
+
